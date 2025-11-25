5142eefff9c301e547029840283272f6d277b7a7
Muhammad Iqbal Ababil <ababiliqbalmuhammad@gmail.com>
2025-11-25 18:51:52 +0700

    Memodifikasi migrasi user dan model user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'marketing', 'manager', 'jamaah'])->default('jamaah');
             $table->rememberToken();
             $table->timestamps();
         });
